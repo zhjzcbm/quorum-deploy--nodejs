155f9ed56bc7f54c6053518f3bce8a8d1da2e8cc
zhjzcbm <zhjzcbm@github.com>
2019-07-24 09:31:05 +0800

    2019.7.24

---
+++ solidity/test.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.0;
+
+contract test {
+    string public x = "hello";
+    uint public z;
+    constructor(uint256 _z)public{
+        z = _z;
+    }
+    function Show()public view returns(string memory){
+      return x;
+	}
+    function Set(string memory z ) public {
+        x = z;
+    }
+
+}
+
